6b37d21b55c039e425d0fc49fd721b94e8c3ed11
ingslh <ingslh@foxmail.com>
2022-09-17 18:47:10 +0800

    Modify directory structure

--- opengl_texture/src/main.cpp
+++ opengl_texture/src/main.cpp
@@ -104,7 +104,7 @@ int main()
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
     // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-    unsigned char *data = stbi_load("../container.jpeg", &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load("../../resources/container.jpeg", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -126,7 +126,7 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
-    data = stbi_load("../awesomeface.png", &width, &height, &nrChannels, 0);
+    data = stbi_load("../../resources/awesomeface.png", &width, &height, &nrChannels, 0);
     if (data)
     {
         // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
