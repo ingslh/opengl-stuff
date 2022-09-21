edef2e10ea6893a6525618ade2e0527013993932
ingslh <ingslh@foxmail.com>
2022-09-21 11:39:51 +0800

    Update 4.2.lighting_maps.fs

--- opengl_lightmap/4.2.lighting_maps.fs
+++ opengl_lightmap/4.2.lighting_maps.fs
@@ -26,13 +26,13 @@ uniform Light light;
 void main()
 {
     // ambient
-    vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
+    vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;//环境光等同于漫反射光
   	
     // diffuse 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;  
+    float diff = max(dot(norm, lightDir), 0.0);//对norm和lightDir向量进行点乘(确定夹角)，计算光源对当前片段实际的漫发射影响
+    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb; //光的颜色*漫反射分量*纹理颜色   
     
     // specular
     vec3 viewDir = normalize(viewPos - FragPos);
