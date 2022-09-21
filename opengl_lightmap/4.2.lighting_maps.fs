b0989417156a5a06c99760b865c6363b683d87c0
ingslh <ingslh@foxmail.com>
2022-09-21 11:58:10 +0800

    Update 4.2.lighting_maps.fs

--- opengl_lightmap/4.2.lighting_maps.fs
+++ opengl_lightmap/4.2.lighting_maps.fs
@@ -30,13 +30,13 @@ void main()
   	
     // diffuse 
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(light.position - FragPos);
+    vec3 lightDir = normalize(light.position - FragPos);//指向光源方向
     float diff = max(dot(norm, lightDir), 0.0);//对norm和lightDir向量进行点乘(确定夹角)，计算光源对当前片段实际的漫发射影响
     vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb; //光的颜色*漫反射分量*纹理颜色   
     
     // specular
-    vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);  
+    vec3 viewDir = normalize(viewPos - FragPos);//计算视线方向向量
+    vec3 reflectDir = reflect(-lightDir, norm);//对lightDir向量进行了取反,reflect函数要求第一个向量是从光源指向片段位置的向量   
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;  
         
