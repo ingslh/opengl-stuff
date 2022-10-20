35aaa1b25cb1f9efb92e05204ff0cec8bdb07c44
liuzh <liuzh@300624.cn>
2022-10-20 10:12:50 +0800

    修改贝塞尔插值中的相关逻辑

--- render_grap/src/bezier_generator.hpp
+++ render_grap/src/bezier_generator.hpp
@@ -19,15 +19,15 @@ public:
     for(size_t i = 0; i < vert_nums; i++){
       BezierCluster cluster;
       cluster.start = vert[i];
-      cluster.out = out[i];
+      cluster.out = out[i] + cluster.start;
 
       if(i == vert_nums -1){
         cluster.end = vert[0];
-        cluster.in = in[0];
+        cluster.in = in[0] + cluster.end;
       }
       else{
         cluster.end = vert[i + 1];
-        cluster.in = in[i + 1];
+        cluster.in = in[i + 1] + cluster.end;
       }
       cluster_list.emplace_back(cluster);
     }
@@ -38,8 +38,8 @@ public:
       }
       else{ //need to generate bezier
         auto delta = 1.0/float(segments_);
-        for(auto i = 0; i < segments_; i++){
-          auto t = delta * float(i);
+        for(unsigned int i = 0; i < segments_; i++){
+          float t = static_cast<float>(delta * float(i));
           vec2 p;
           p.x = (1 - t) * (1 - t) * (1 - t) * it.start.x + 3 * t * (1 - t) * (1 - t)* it.out.x + 3 * t*t* (1 - t)* it.in.x + t * t * t * it.end.x;
           p.y = (1 - t) * (1 - t) * (1 - t) * it.start.y + 3 * t * (1 - t) * (1 - t)* it.out.y + 3 * t*t* (1 - t)* it.in.y + t * t * t * it.end.y;
